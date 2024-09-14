4262fac92d6d5f27bdb43eb89ea7751c282609c5
laizhangtu <154239679+laizhangtu@users.noreply.github.com>
2024-09-14 11:23:15 +0800

    lift-limits

--- AutoLoginLite.py
+++ AutoLoginLite.py
@@ -1,31 +1,41 @@
 # -*- coding: utf-8 -*-
-"""
-   @Author: Kang
-   @Version 1.1
-   @File: AutoLoginLite.py
-   @CreateTime: 2021-09-05 13:22:51
-   @Software: PyCharm
-"""
 from time import sleep
-
 import requests
-
 
 # 自动登录
 def autoLogin():
     url = "http://10.255.0.19/drcom/login"
     try:
         once = requests.get(url=url,
-                            params={'callback': 'dr1003', 'DDDDD': "201930****@****", 'upass': "*******",
+                            params={'callback': 'dr1003', 'DDDDD': "2023307506@cmcc", 'upass': "AUST@2004",
                                     '0MKKey': 123456})
-        sleep(3)
+        sleep(2)
         twice = requests.get(url=url,
-                             params={'callback': 'dr1003', 'DDDDD': "201930****@****", 'upass': "*******",
+                             params={'callback': 'dr1003', 'DDDDD': "2023307506@cmcc", 'upass': "AUST@2004",
                                      '0MKKey': 123456})
         return "自动登陆成功"
     except:
         return "发生异常"
 
+def get_public_ip():
+    try:
+        response = requests.get('http://ifconfig.me/ip')  # 发送GET请求获取IP地址
+        if response.status_code == 200:
+            return response.text.strip()  # 返回文本形式的IP地址
+        else:
+            print("Error: Unable to fetch IP address.")
+            return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 if __name__ == '__main__':
     autoLogin()
+    while True:
+        if get_public_ip() == "211.141.172.154":  #你的常用IP地址    
+            autoLogin()
+            public_ip = get_public_ip()
+            print(f"Your public IP address is: {public_ip}")
+        else :
+            print(get_public_ip())
+            break
